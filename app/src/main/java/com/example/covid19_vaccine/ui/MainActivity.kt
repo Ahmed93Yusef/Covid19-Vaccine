708c46062eca494106693b8b6c069a2d231b72fc
Ahmed Yusef <ahmed.yusef.1993@gmail.com>
2021-08-16 12:24:07 +0300

    create base activity class

---
+++ app/src/main/java/com/example/covid19_vaccine/ui/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.covid19_vaccine.ui
+import android.view.LayoutInflater
+import com.example.covid19_vaccine.databinding.ActivityMainBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class MainActivity: BaseActivity<ActivityMainBinding>() {
+    override val LOG_TAG: String = "MAIN_ACTIVITY"
+    override val bindingInflater: (LayoutInflater) -> ActivityMainBinding = ActivityMainBinding::inflate
+
+    override fun setup() {
+        openFile()
+    }
+    override fun addCallbacks() {
+    }
+
+    private fun openFile() {
+        val inputStream = assets.open("country_vaccinations.csv")
+        val buffer = BufferedReader(InputStreamReader(inputStream))
+        buffer.forEachLine {
+            log(it)
+        }
+    }
+
+
+
+}
