21ffab5446d2965b76fb1ed60a55110eb4bd80dc
Ahmed Yusef <ahmed.yusef.1993@gmail.com>
2021-08-19 04:20:35 +0300

    create bottom navigation bar

--- app/src/main/java/com/example/covid19_vaccine/ui/MainActivity.kt
+++ app/src/main/java/com/example/covid19_vaccine/ui/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.covid19_vaccine.ui
 import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
+import com.example.covid19_vaccine.R
 import com.example.covid19_vaccine.data.DataManger
 import com.example.covid19_vaccine.databinding.ActivityMainBinding
 import com.example.covid19_vaccine.util.CsvParser
@@ -7,6 +9,10 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 class MainActivity: BaseActivity<ActivityMainBinding>() {
+    private val myHomeFragment = HomeFragment()
+    private val mySearchFragment = SearchFragment()
+    private val myStatisticFragment = StatisticFragment()
+    private val myAboutFragment = AboutFragment()
     override val LOG_TAG: String = "MAIN_ACTIVITY"
     override val bindingInflater: (LayoutInflater) -> ActivityMainBinding = ActivityMainBinding::inflate
 
@@ -14,7 +20,49 @@ class MainActivity: BaseActivity<ActivityMainBinding>() {
         parseFile()
     }
     override fun addCallbacks() {
+        initSubView()
+        binding?.bottomNavigationView!!.setOnItemSelectedListener {
+            item ->
+            when(item.itemId){
+                R.id.homePage -> {
+                    replaceFragment(myHomeFragment)
+                    true
+                }
+                R.id.searchPage -> {
+                    replaceFragment(mySearchFragment)
+                    true
+                }
+                R.id.statisticPage -> {
+                    replaceFragment(myStatisticFragment)
+                    true
+                }
+                R.id.aboutPage -> {
+                    replaceFragment(myAboutFragment)
+                    true
+                }
+                else -> false
 
+            }
+        }
+
+
+    }
+
+    private fun initSubView() {
+        addFragment(HomeFragment())
+
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_container, fragment)
+        transaction.commit()
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
 
     }
 
