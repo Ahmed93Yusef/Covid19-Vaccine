756edf3a77ff4f2086bebf8bb65919ad269e2ae4
Ahmed Yusef <ahmed.yusef.1993@gmail.com>
2021-08-23 00:23:09 +0300

    update data csv

--- app/src/main/java/com/example/covid19_vaccine/util/Constant.kt
+++ app/src/main/java/com/example/covid19_vaccine/util/Constant.kt
@@ -4,7 +4,7 @@ object Constant {
     object ColumnIndex{
         const val COUNTRY = 0
         const val DATE = 2
-        const val PEOPLE_VACCINATED = 4
+        const val PEOPLE_VACCINATED = 3
         const val PEOPLE_FULLY_VACCINATED = 5
         const val DAILY_VACCINATIONS = 7
         const val PEOPLE_VACCINATED_PER_HUNDRED = 9
