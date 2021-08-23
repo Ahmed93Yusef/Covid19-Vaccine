a932a1aa6514cd6c4209abe8aeaa6ae62a40db82
Ahmed Yusef <ahmed.yusef.1993@gmail.com>
2021-08-23 13:38:55 +0300

    update new fragment vaccine type

--- app/src/main/java/com/example/covid19_vaccine/ui/fragment/VaccineTypeFragment.kt
+++ app/src/main/java/com/example/covid19_vaccine/ui/fragment/VaccineTypeFragment.kt
@@ -1,17 +1,18 @@
-//package com.example.covid19_vaccine.ui.fragment
-//
-//
-//import android.view.LayoutInflater
-//
-//class VaccineTypeFragment : BaseFragment<VaccineTypeFragment>(){
-//    override val bindingInflater: (LayoutInflater) -> VaccineTypeFragment = VaccineTypeFragment::getLayoutInflater
-//
-//    override fun setup() {
-//    }
-//
-//    override fun addCallBack() {
-//    }
-//
-//
-//}
-//
+package com.example.covid19_vaccine.ui.fragment
+
+
+import android.view.LayoutInflater
+import com.example.covid19_vaccine.databinding.FragmentVaccineTypeBinding
+
+class VaccineTypeFragment : BaseFragment<FragmentVaccineTypeBinding>(){
+    override val bindingInflater: (LayoutInflater) -> FragmentVaccineTypeBinding = FragmentVaccineTypeBinding::inflate
+
+    override fun setup() {
+    }
+
+    override fun addCallBack() {
+    }
+
+
+}
+
