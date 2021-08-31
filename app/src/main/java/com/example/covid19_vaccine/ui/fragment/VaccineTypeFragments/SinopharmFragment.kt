40aa21d26e6054028e1586dcd7f9639c10fc48e7
Nooralden <nooraldenakel@gmail.com>
2021-08-31 13:37:57 +0300

    complete the Vaccine type screen by add for each type separated screen deleted other stuff in "Drawable" we don't need it

---
+++ app/src/main/java/com/example/covid19_vaccine/ui/fragment/VaccineTypeFragments/SinopharmFragment.kt
@@ -0,0 +1,20 @@
+package com.example.covid19_vaccine.ui.fragment.VaccineTypeFragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.covid19_vaccine.R
+import com.example.covid19_vaccine.databinding.FragmentSinopharmBinding
+import com.example.covid19_vaccine.ui.fragment.BaseFragment
+
+class SinopharmFragment : BaseFragment<FragmentSinopharmBinding>() {
+    override val bindingInflater: (LayoutInflater) -> FragmentSinopharmBinding = FragmentSinopharmBinding::inflate
+
+
+    override fun setup() {}
+    override fun addCallBack() {}
+
+
+}
---
+++ app/src/main/java/com/example/covid19_vaccine/ui/fragment/VaccineTypeFragments/Sputing_v_Fragment.kt
@@ -0,0 +1,18 @@
+package com.example.covid19_vaccine.ui.fragment.VaccineTypeFragments
+
+import android.view.LayoutInflater
+import com.example.covid19_vaccine.databinding.FragmentSputnikVBinding
+import com.example.covid19_vaccine.ui.fragment.BaseFragment
+
+class Sputing_v_Fragment : BaseFragment<FragmentSputnikVBinding>() {
+    override val bindingInflater: (LayoutInflater) -> FragmentSputnikVBinding = FragmentSputnikVBinding::inflate
+
+
+    override fun setup() {
+
+    }
+
+    override fun addCallBack() {
+
+    }
+}
