1f56fe526391a004aa693d998d12dfe3b2bce0f5
Nooralden <nooraldenakel@gmail.com>
2021-08-31 13:52:20 +0300

    complete the Vaccine type screen by add for each type separated screen deleted other stuff in "Drawable" we don't need it

---
+++ app/src/main/java/com/example/covid19_vaccine/ui/fragment/VaccineTypeFragments/PfizerFragment.kt
@@ -0,0 +1,17 @@
+package com.example.covid19_vaccine.ui.fragment.VaccineType
+
+import android.view.LayoutInflater
+import com.example.covid19_vaccine.databinding.FragmentPfizerBinding
+import com.example.covid19_vaccine.ui.fragment.BaseFragment
+
+class PfizerFragment : BaseFragment<FragmentPfizerBinding>() {
+    override val bindingInflater: (LayoutInflater) -> FragmentPfizerBinding = FragmentPfizerBinding::inflate
+    override fun setup() {
+
+    }
+
+    override fun addCallBack() {
+    }
+
+
+}
